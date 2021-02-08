ab333dac7399e37636bc6f0db0aefb652ad806cc
Manmohan Singh <manmohan15214@gmail.com>
2021-02-08 15:54:45 +0530

    Corrected sol

--- CF 700 Div 2/B.cpp
+++ CF 700 Div 2/B.cpp
@@ -11,49 +11,33 @@ typedef long long ll;
 
 int main()
 {
-    int T;
+    ll T;
     cin >> T;
     while (T--)
     {
-        int A, B, N;
+        ll A, B, N;
         cin >> A >> B >> N;
 
-        int pow[N], hlt[N];
-
-        for (int i = 0; i < N; i++)
+        ll pow[N], hlt[N];
+        ll maxP = 0;
+        for (ll i = 0; i < N; i++)
         {
             cin >> pow[i];
+            maxP = max(maxP, pow[i]);
         }
-        for (int i = 0; i < N; i++)
+        for (ll i = 0; i < N; i++)
         {
             cin >> hlt[i];
+            B -= ((hlt[i] + A - 1) / A) * pow[i];
         }
 
-        int dead = 0;
-
-        for (int i = 0; i < N; i++)
+        if (B + maxP > 0)
         {
-
-            while (hlt[i] > 0 && B > 0)
-            {
-                B -= pow[i];
-                hlt[i] -= A;
-            }
-
-            if (hlt[i] <= 0)
-                dead++;
-
-            if (B <= 0)
-                break;
-        }
-
-        if (dead < N)
-        {
-            cout << "NO" << endl;
+            cout << "YES" << endl;
         }
         else
         {
-            cout << "YES" << endl;
+            cout << "NO" << endl;
         }
     }
 }
